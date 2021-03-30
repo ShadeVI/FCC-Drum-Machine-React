26e5d6c3a4eedae72be261315610b864c77c98e5
ShadeVI <manuel.t.shade@gmail.com>
2021-03-30 01:47:14 +0100

    added link to github

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
     <div id="drum-machine" className="container">
       <Header />
       <DrumBase />
+      <footer className="fixed-bottom text-center mb-2">
+        <small>
+          Developed by{" "}
+          <a className="link-primary" href="https://github.com/ShadeVI">
+            ShadeVI
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }
